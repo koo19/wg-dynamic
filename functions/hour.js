817a8ca1a3f0de8730791ded22ef36d292108526
hugo.koo <hokdung@outlook.com>
2025-05-15 15:22:07 +0800

    one hour quene

---
+++ functions/hour.js
@@ -0,0 +1,27 @@
+import { getConfigSet, getTimeInfo, genWgurl } from './utils';
+
+export async function onRequest(context) {
+  const { request, env } = context;
+  const url = new URL(request.url);
+  const providedAccessKey = url.searchParams.get("accesskey");
+
+  const configSet = getConfigSet(env, providedAccessKey);
+
+  if (!configSet) {
+    return new Response("Unauthorized: Invalid access key.", {
+      status: 401,
+      headers: { "Content-Type": "text/plain; charset=utf-8" },
+    });
+  }
+
+  const { hoursOfYear } = getTimeInfo();
+
+  let hoursSequence = [];
+  for (let i = 0; i < 1; i++) {
+    hoursSequence.push(genWgurl(configSet, hoursOfYear + i));
+  }
+
+  return new Response(hoursSequence.join("\n"), {
+    headers: { "Content-Type": "text/plain; charset=utf-8" },
+  });
+}
